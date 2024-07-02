bf539813bcd83f4d10e5badcb39a27e5180f2415
Fabio Fiorellato <flowerside@gmail.com>
2024-07-02 09:21:54 +0200

    Updating catalogue data

--- initialize_data.R
+++ initialize_data.R
@@ -3,10 +3,11 @@ library(iccat.dev.data)
 META = list(LAST_UPDATE = as.Date(Sys.Date(), format = "%Y-%M-%d"))
 save(list = "META", file = "./shiny/META.RData")
 
-CA_ALL = catalogue.fn_genT1NC_CatalSCRS(species_codes    = NULL,
-                                        stock_area_codes = NULL,
-                                        year_from        = 1950,
-                                        year_to          = NA,
-                                        db_connection    = DB_STAT())
+CA_ALL = catalogue.fn_genT1NC_CatalSCRS(species_codes      = NULL,
+                                        stock_area_codes   = NULL,
+                                        year_from          = 1950,
+                                        year_to            = NA,
+                                        db_t1_connection   = DB_T1(),
+                                        db_stat_connection = DB_STAT())
 
 save(list = "CA_ALL", file = "./shiny/CA_all.RData")
