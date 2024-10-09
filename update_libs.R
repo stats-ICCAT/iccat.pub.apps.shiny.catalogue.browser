4886e61da1610d6ad9fe220a830b37e5d6905323
Fabio Fiorellato <fabio@fiorella.to>
2024-10-09 14:39:04 +0200

    Fixing update libs script

--- update_libs.R
+++ update_libs.R
@@ -2,9 +2,9 @@ library(devtools)
 
 GITHUB_AUTH_TOKEN = Sys.getenv("GITHUB_AUTH_TOKEN")
 
-devtools::install_github("iccat-r-tests/libs/public/iccat.pub.base", auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
-devtools::install_github("iccat-r-tests/libs/public/iccat.pub.data", auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
-devtools::install_github("iccat-r-tests/libs/public/iccat.pub.aes",  auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
-devtools::install_github("iccat-r-tests/libs/public/iccat.pub.viz",  auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
+devtools::install_github("stats-ICCAT/iccat.pub.base", auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
+devtools::install_github("stats-ICCAT/iccat.pub.data", auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
+devtools::install_github("stats-ICCAT/iccat.pub.aes",  auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
+devtools::install_github("stats-ICCAT/iccat.pub.viz",  auth_token = GITHUB_AUTH_TOKEN, dependencies = FALSE)
 
 q(save = "no")
